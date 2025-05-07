005b91f56a30489d1d0154484696a4de8c488384
jyooni99 <berry3891@naver.com>
2025-05-08 01:12:59 +0900

    feat: 로그인폼 유효성 검사 함수 생성

---
+++ src/utils/validators.ts
@@ -0,0 +1,26 @@
+// 유효성 검사 함수
+
+function validateEmail(value: string): string | undefined {
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  if (!value.length) return '이메일을 입력하세요.';
+  if (!emailRegex.test(value)) return '이메일 주소가 유효하지 않습니다.';
+  return undefined;
+}
+
+function validatePassword(value: string): string | undefined {
+  const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/;
+  if (!value.length) return '비밀번호를 입력하세요.';
+  if (!passwordRegex.test(value))
+    return '비밀번호를 확인해주세요. (최소 8자, 영문,숫자,특수문자 1개 이상 포함)';
+
+  return undefined;
+}
+
+export function loginValidator(values: Record<string, string>): Record<string, string | undefined> {
+  const errors: Record<string, string | undefined> = {};
+
+  errors.email = validateEmail(values.email);
+  errors.password = validatePassword(values.password);
+
+  return errors;
+}
