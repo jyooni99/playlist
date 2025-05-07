872e1f10ef7b37f81dd29e5718edf493fc6d2c06
jyooni99 <berry3891@naver.com>
2025-05-08 00:10:34 +0900

    feat: header css 수정

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -11,18 +11,21 @@ const Header = () => {
     <header>
       <nav className='w-full fixed top-0 bg-white'>
         <div className='w-full flex justify-between items-center py-5 px-5'>
+          {/* 로고 */}
           <div>
             <Link to='/' className='font-bold text-xl'>
               PlayList
             </Link>
           </div>
+
+          {/* 검색창 */}
           <div className='w-1/2 hidden min-[480px]:flex min-[480px]:relative'>
             <input
               type='text'
-              className='w-full outline-none placeholder:text-gray-400 placeholder:font-bold placeholder:text-sm border border-gray-400 pl-4 py-1.5 rounded-full overflow-hidden focus:ring-1 focus:ring-black'
+              className='w-full outline-none placeholder:text-gray-400 placeholder:font-bold placeholder:text-sm border border-gray-400 pl-4 pr-20 py-1.5 rounded-full overflow-hidden focus:ring-1 focus:ring-black'
               placeholder='검색어를 입력하세요'
             />
-            <button className='absolute right-0 w-[70px] h-full cursor-pointer border-l-1 bg-black'>
+            <button className='absolute right-0 px-5 h-full cursor-pointer border-l-1 border-gray-400'>
               검색
             </button>
           </div>
