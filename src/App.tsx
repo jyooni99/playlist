0d6d33b1472bcfbb04cdecbf03a90219b55ce353
jyooni99 <berry3891@naver.com>
2025-05-07 17:33:29 +0900

    chore: react-router-dom 설치 및 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages';
+import Login from './pages/login';
+import SignUp from './pages/signup';
+import Users from './pages/users';
+import ProfileEdit from './pages/profile-edit';
+import NotFound from './pages/not-found';
+
 import './styles/reset.css';
 
 function App() {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} /> {/*동영상 리스트 페이지*/}
+      <Route path='/login' element={<Login />} /> {/* 로그인 페이지 */}
+      <Route path='/signup' element={<SignUp />} /> {/* 회원가입 페이지 */}
+      <Route path='/users' element={<Users />} />
+      <Route path='profile-edit' element={<ProfileEdit />} /> {/* 프로필 수정 페이지 */}
+      <Route path='*' element={<NotFound />} /> {/*Not found*/}
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/users.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom';
+
+const Users = () => {
+  return (
+    <div>
+      Users
+      <Outlet />
+    </div>
+  );
+};
+
+export default Users;
