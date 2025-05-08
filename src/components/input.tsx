2078755c2a4ae38f6ae58bb3b54e2c080b2bd4e8
jyooni99 <berry3891@naver.com>
2025-05-08 15:24:24 +0900

    feat: input 컴포넌트에서 에러 처리

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -5,6 +5,8 @@ type InputProps = {
   value: string;
   placeholder?: string;
   required?: boolean;
+  error?: string;
+  touched?: boolean;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   onBlur?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
@@ -16,9 +18,13 @@ const Input = ({
   value,
   placeholder,
   required,
+  error,
+  touched,
   onChange,
   onBlur,
 }: InputProps) => {
+  const hasError = touched && error;
+
   return (
     <div className='mb-4'>
       {label && (
@@ -28,7 +34,9 @@ const Input = ({
       )}
 
       <input
-        className='mt-2 w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-black'
+        className={`mt-2 w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-1 ${
+          hasError ? 'border-red-500 focus:ring-red-500' : 'border-gray-300 focus:ring-black'
+        }`}
         id={id}
         type={type}
         name={id}
@@ -38,6 +46,8 @@ const Input = ({
         onChange={onChange}
         onBlur={onBlur}
       />
+
+      {hasError && <p className='mt-1 text-sm text-red-600'>{error}</p>}
     </div>
   );
 };
